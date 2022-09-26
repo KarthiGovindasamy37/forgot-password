1e5bdb3bd226070c60ad09c0ac357dbb74150edf
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2022-09-26 13:32:25 +0530

    upd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/forgot' element={<ForgotMail/>}/>
-      <Route path='/reset/:token' element={<PasswordChange/>}/>
+      <Route path='/resetpassword/:token' element={<PasswordChange/>}/>
 
     </Routes>
     </BrowserRouter>
