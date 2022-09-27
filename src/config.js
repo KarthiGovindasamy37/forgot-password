45618509f4420fe1760dc48498ab2f0dd60264e8
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2022-09-27 17:44:15 +0530

    upd

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const env={
-    api:"http://localhost:3001"
-    // api:"https://forgot-passwor.herokuapp.com"
+    // api:"http://localhost:3001"
+    api:"https://forgot-passwor.herokuapp.com"
 }
