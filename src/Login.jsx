f77451fae2768694324b93cebb1599fd6cfc344c
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2022-09-27 18:02:39 +0530

    upd

--- src/Login.jsx
+++ src/Login.jsx
@@ -75,7 +75,7 @@ let formik=useFormik({
               </div>
               <div className="d-flex justify-content-end mt-3 mb-3">
                 <h6 className="">
-                  Don't have an account? <Link to="/create">Create account</Link>
+                  Don't have an account? <Link to="/create">Sign up</Link>
                 </h6>
               </div>
             </div>
